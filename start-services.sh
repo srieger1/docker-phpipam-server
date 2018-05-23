651a7c7012e6b71d612e9aff4bd4609e347c3793
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2018-05-23 22:10:52 +0200

    improved startup file

--- start-services.sh
+++ start-services.sh
@@ -2,6 +2,10 @@
 
 # when using VOLUME in Dockerfile, /var/lib/mysql gets owned by user of the host system
 chown -R mysql:mysql /var/lib/mysql
+sleep 1
+
 service mysql start
+
 service apache2 start
+
 bash
